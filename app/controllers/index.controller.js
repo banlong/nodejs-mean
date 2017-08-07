2db29822b7bf954f3941c9818990c1fed7b0eaef
nghiepngo <nghiepnds@yahoo.com>
2017-08-06 17:26:37 -0700

    1. Try mongod

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -6,16 +6,16 @@
 
 /* CONTROLLER GET home page */
 module.exports.getIndex = function(req, res, next){
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Express MVC',
-      articles: articles
-    });
-  });
-
-
-  // res.render('index', {
-  //   title: 'Express MVC'
+  // Article.find(function (err, articles) {
+  //   if (err) return next(err);
+  //   res.render('index', {
+  //     title: 'Express MVC',
+  //     articles: articles
+  //   });
   // });
+
+
+  res.render('index', {
+    title: 'Express MVC'
+  });
 };
