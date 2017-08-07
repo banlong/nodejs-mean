b21678dcabab2c11e813996f53a284b88e2799a3
nghiepngo <nghiepnds@yahoo.com>
2017-08-06 17:00:56 -0700

    1. Add env to upload to Keroku

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -6,11 +6,16 @@
 
 /* CONTROLLER GET home page */
 module.exports.getIndex = function(req, res, next){
-  Article.find(function (err, articles) {
-    if (err) return next(err);
-    res.render('index', {
-      title: 'Express MVC',
-      articles: articles
-    });
+  // Article.find(function (err, articles) {
+  //   if (err) return next(err);
+  //   res.render('index', {
+  //     title: 'Express MVC',
+  //     articles: articles
+  //   });
+  // });
+
+
+  res.render('index', {
+    title: 'Express MVC'
   });
 };
