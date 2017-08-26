de8767803b715466a610453cbaec9dc7ff779555
nghiepngo <nghiepnds@yahoo.com>
2017-08-26 01:24:05 -0700

    Using API for Add Review

--- app/controllers/locations.controller.js
+++ app/controllers/locations.controller.js
@@ -29,7 +29,89 @@ module.exports.homelist = function(req, res){
   );
 };
 
-var renderHomepage = function (req, res, responseBody) {
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req, res){
+  getLocationInfo(req, res, function (req, res, responseData) {
+    renderDetailPage(req, res, responseData);
+  })
+
+};
+
+/* GET 'Add review' page */
+module.exports.addReview = function(req, res){
+  getLocationInfo(req, res, function (req, res, responseData) {
+    renderReviewForm(req, res, responseData);
+  })
+
+};
+
+/* POST 'Add review' page */
+module.exports.doAddReview = function(req, res){
+
+  var requestOptions, path, locationid, postdata;
+  locationid = req.params.locationid;
+  console.log(locationid);
+  path = "/api/locations/" + locationid + '/reviews';
+  postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+
+  requestOptions = {
+    url : config.apiServer + path,
+    method : "POST",
+    json : postdata
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      if (response.statusCode === 201) {
+        res.redirect('/location/' + locationid);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    }
+  );
+};
+
+function renderReviewForm(req, res, responseData) {
+  console.log(req.params.locationid);
+  console.log(responseData);
+  res.render('location-review-form', {
+    title: "Review " + responseData.name + " on Loc8r",
+    pageHeader: {
+      title: "Review " + responseData.name
+    }
+  });
+}
+
+var getLocationInfo = function (req, res, callback) {
+  var requestOptions, path;
+  path = "/api/locations/" + req.params.locationid;
+  requestOptions = {
+    url : config.apiServer + path,
+    method : "GET",
+    json : {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      if (response.statusCode === 200) {
+        data.coords = {
+          lng : body.coords[0],
+          lat : body.coords[1]
+        };
+        callback(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    }
+  );
+};
+
+function renderHomepage(req, res, responseBody) {
   var message;
   if (!(responseBody instanceof Array)) {
     message = "API lookup error";
@@ -48,46 +130,27 @@ var renderHomepage = function (req, res, responseBody) {
       strapline: 'Find places to work with wifi near you!'
     },
     sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about.Perhaps with coffee, " +
-             "cake or a  pint ? Let Loc8r help you find the place you're looking for.",
+    "cake or a  pint ? Let Loc8r help you find the place you're looking for.",
     locations: locations,
     message: message
   });
-};
+}
 
-/* GET 'Location info' page */
-module.exports.locationInfo = function(req, res){
-  var locationId = req.params.locationid;
-  var requestOptions, path;
-  path = '/api/locations/' + locationId;
-  requestOptions = {
-    url: config.apiServer + path,
-    method: "GET",
-    json: {}
-  };
-
-  //send request to Rest API
-  request(
-    requestOptions,
-    function (err, response, body) {
-      var data = body;
-      data.coords = {
-        lng : body.coords[0],
-        lat : body.coords[1]
-      };
-      renderDetailPage(req, res, data);
-    }
-  );
-};
-
-/* GET 'Add review' page */
-module.exports.addReview = function(req, res){
-  res.render('location-review-form', {
-    title: 'Review Starcups on Loc8r',
-    pageHeader: {
-      title: 'Review Starcups'
-    }
-  });
-};
+function renderDetailPage(req, res, locDetail) {
+    locDetail.rating = convertRating(locDetail.rating);
+    locDetail.reviews = convertMultipleRatings(locDetail.reviews);
+    locDetail.reviews = formatDates(locDetail.reviews);
+    console.log(locDetail);
+    res.render('location-info', {
+      title: locDetail.name,
+      pageHeader: {title: locDetail.name},
+      sidebar: {
+        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+        callToAction: 'If you\'ve been and you like it - or if you don\'t please leave a review to help other people just like you.'
+      },
+    location: locDetail
+    });
+}
 
 // Create a rating array with 1 is full, 0 is empty
 function convertMultipleRatings(data) {
@@ -102,6 +165,20 @@ function convertMultipleRatings(data) {
   return data;
 }
 
+function formatDates(data) {
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  for (var i = 0; i < data.length; i++){
+    var dateVal  = new Date(data[i].createdOn);
+    var day = dateVal.getDate();
+    var month = dateVal.getMonth();
+    var monthStr = months[month];
+    var year = dateVal.getFullYear();
+    var formattedDate = day + " " + monthStr + " " + year;
+    data[i].createdOn = formattedDate;
+  }
+  return data;
+}
+
 function convertRating(rate) {
   var rateVal = [false,false,false,false,false];
   if (rate <= 0) return rateVal;
@@ -110,37 +187,4 @@ function convertRating(rate) {
   }
   return rateVal;
 
-}
-
-module.exports.article = function(req, res, next){
-  Location.find(function (err, locations) {
-    if (err) return next(err);
-    res.render('locationlist', {
-      title: 'Create Article'
-    });
-  });
-};
-
-module.exports.postArticle = function(req, res, next) {
-  var url = req.body.url;
-  var title = req.body.title;
-  var content = req.body.content;
-  var location = new Location({});
-  location.save();
-};
-
-
-function renderDetailPage(req, res, locDetail) {
-    locDetail.rating = convertRating(locDetail.rating);
-    locDetail.reviews = convertMultipleRatings(locDetail.reviews);
-    console.log(locDetail);
-    res.render('location-info', {
-      title: locDetail.name,
-      pageHeader: {title: locDetail.name},
-      sidebar: {
-        context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-        callToAction: 'If you\'ve been and you like it - or if you don\'t please leave a review to help other people just like you.'
-      },
-    location: locDetail
-    });
 }
--- app/routes/index.router.js
+++ app/routes/index.router.js
@@ -13,7 +13,8 @@ module.exports = function (app) {
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
