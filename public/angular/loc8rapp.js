bcb6cbfcd4d1b901172870d93097511c19593e74
nghiepngo <nghiepnds@yahoo.com>
2017-08-31 00:39:54 -0700

    - Create format custom filter for distance - Create component for location in location list

--- public/angular/loc8rapp.js
+++ public/angular/loc8rapp.js
@@ -9,20 +9,43 @@
     $interpolateProvider.endSymbol('}]}');
   });
 
+  //Add custom filter
+  app.filter('formatDistance', formatDistance);
+
+  //Location component
+  app.component("locationBar", {
+    templateUrl:'/angular/templates/location.html',
+    bindings:{
+      location: '='    //this is location inf
+    },
+    controllerAs: "model",
+    controller: function () {
+      var model;
+      this.$onInit = function() {
+        model = this.location;
+
+        var rateVal = [false,false,false,false,false];
+        for (var j = 0; j < model.rating; j++){
+            rateVal[j] = true;
+        }
+        model.rating = rateVal;
+      };
+    }
+  });
 
   app.controller('locationListCtrl', function ($scope) {
     $scope.data = {
       locations: [{
         name: 'Burger Queen',
         address: '125 High Street, Reading, RG6 1PS',
-        rating: convertRating(3),
+        rating: 3,
         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
         distance: '0.3',
         _id: '5370a35f2536f6785f8dfb6a'
       },{
         name: 'Costy',
         address: '125 High Street, Reading, RG6 1PS',
-        rating: convertRating(5),
+        rating: 5,
         facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
         distance: '0.8',
         _id: '5370a35f2536f6785f8dfb6a'
@@ -37,6 +60,28 @@
       rateVal[j] = true;
     }
     return rateVal;
+  }
+
+  function _isNumeric (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+
+ function formatDistance() {
+    return function (distance) {
+      var numDistance, unit;
+      if (distance && _isNumeric(distance)) {
+        if (distance > 1) {
+          numDistance = parseFloat(distance).toFixed(1);
+          unit = 'km';
+        } else {
+          numDistance = parseInt(distance * 1000,10);
+          unit = 'm';
+        }
+        return numDistance + unit;
+      } else {
+        return "?";
+      }
+    };
   }
 })();
 
